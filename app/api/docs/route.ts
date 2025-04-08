f008b6ab9cd35fb28b7ce4115785b16af4bfccf9
ilialor <info@ateira.online>
2025-04-08 11:11:36 +0300

    free api docs

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -1,11 +1,10 @@
 import { getApiDocs } from '@/lib/swagger';
 import { NextResponse } from 'next/server';
 import { withCors, corsResponse } from '@/lib/cors';
-import { withApiAuth } from '@/lib/api-auth';
 
 /**
  * API endpoint to serve Swagger specification
  */
-export const GET = withCors(withApiAuth(async function GET() {
+export const GET = withCors(async function GET() {
   return corsResponse(getApiDocs());
-}));
+});
